4859ff605620df244ece21a86fa51115263f555f
rukja <orukja@gmail.com>
2026-07-03 11:58:36 +1000

    added yom quote croatia

--- docs/content.js
+++ docs/content.js
@@ -288,7 +288,8 @@ window.SITE_CONTENT = {
             { title: `"i fear we need an apology for the apology"`, description: "u/awordalonee. Context: Taylor Townsend's apology video for mocking Chinese food." },
             { title: `"I am sorry to play the role of Reviewer #2."`, description: `Anonymous. Context: Reviewer #1 in the peer-review report for "Harms of introduced large herbivores outweigh benefits to native biodiversity".` },
             { title: `"Profound but technically meaningless."`, description: "Jeff. Context: Community S1 E6, I forgot what specifically." },
-            { title: `"Invisitibility is a superpower."`, description: "Banksy (2010)."}
+            { title: `"Invisitibility is a superpower."`, description: "Banksy (2010)."},
+            { title: `"they put a microchip in the ball? Why’d they give it a Covid vaccine ?"`, description: "Vyom Pawar (2026). Context: VAR disallowing Croatia's equalizing goal in WC R32"}
           ]
         }
       ]
